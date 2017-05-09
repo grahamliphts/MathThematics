fb35f76e64cbc1310c0b00664fd8a7ac5f1ba70a
DESKTOP-JVPO5O1\Ravonyx <ravonyxa@gmail.com>
2017-05-09 14:15:34 +0200

    add imgui

--- maths/maths.cpp
+++ maths/maths.cpp
@@ -2,9 +2,14 @@
 //
 
 #include "stdafx.h"
-#include <GL\glew.h>
+#include"imgui.h"
+#include "imgui_impl_glfw_gl3.h"
+#include "GL\gl3w.h"
+
 #include <GLFW\glfw3.h>
 #include <glm\glm.hpp>
+
+
 //#include <>
 
 using namespace glm;
@@ -31,17 +36,41 @@ int main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window); // Initialize GLEW
-	glewExperimental = true; // Needed in core profile
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		return -1;
-	}
+
+	gl3wInit();
+	// Setup ImGui binding
+	ImGui_ImplGlfwGL3_Init(window, true);
+
 
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
 	do {
 		// Draw nothing, see you in tutorial 2 !
+
+
+			ImGui_ImplGlfwGL3_NewFrame();
+
+			ImGui::Begin("Convex Hull & Voronoi");
+			ImGui::Text("This window is here to use the application!");
+
+			ImGui::Columns(2, "mixed");
+			ImGui::Separator();
+			//ImGui::Checkbox("Triangulation", &triangulationEnabled);
+			//ImGui::Checkbox("Enhance Triangulation", &enhanceTriangulation);
+
+			ImGui::NextColumn();
+			//ImGui::Checkbox("Voronoi", &voronoiEnabled);
+			//ImGui::Checkbox("Enhance Voronoi", &enhanceVoronoi);
+
+			ImGui::Columns(1);
+			ImGui::Separator();
+
+			ImGui::Columns(2, "mixed");
+			ImGui::Separator();
+
+			ImGui::Text("Graphic Manipulation");
+			ImGui::Render();
 
 		// Swap buffers
 		glfwSwapBuffers(window);
@@ -51,6 +80,9 @@ int main()
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
 		glfwWindowShouldClose(window) == 0);
 
+	// Cleanup
+	ImGui_ImplGlfwGL3_Shutdown();
+	glfwTerminate();
 
     return 0;
 }
