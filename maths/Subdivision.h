836d5b63feb2b4a727ba326126abdc71134af9c8
Kevin Rey <k.rey@hotmail.fr>
2017-05-11 18:02:30 +0200

    Update Subdivision

--- maths/Subdivision.h
+++ maths/Subdivision.h
@@ -1,14 +1,16 @@
 #pragma once
 
+#define _USE_MATH_DEFINES
+
 #include <glm\glm.hpp>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <vector>
 
+
 void SubdivisionKobbelt(std::vector<float> &vertex, std::vector<int> &faces, int iteration);
 
-
 std::vector<glm::vec3> getVertexInFaces(std::vector<float> vertex, std::vector<int> faces, int idFace);
-
 std::vector<int> getAdjacentFaces(std::vector<int> faces, int idFace);
+float Perturbation(int n);
